a1dd657711669617bf244cfb42f59cd4e39aec28
4nemi <4nemi.aaa@gmail.com>
2023-06-15 15:33:21 +0900

    fix config

--- src/config.py
+++ src/config.py
@@ -1,17 +1,26 @@
 from transformers import AutoTokenizer, AutoConfig
-PARTICIPANT = "亀山直生"
+PARTICIPANT = ""
 MODEL_NUM = 1
-TEST_NAME = '空の中 (角川文庫)'
-TRAIN_FILE = '../input/review1_cleaned.csv'
+EXPRRIMENT = 'kfold' #stratified kfold or group kfold or holdout
+TARGET = 'p_rating and diff' #rating or diff
+TEST_NAME = []
+TRAIN_FILE = '../input/review1_cleaned_2.csv'
 
 DEBUG = False
 EPOCHS = 10
+TRN_FOLD = [0, 1, 2, 3]
 if DEBUG:
     EPOCHS = 2
+    TRN_FOLD = [0]
+    
+ALPHA = 0.5
+
+WANDB = True
 
 MODEL = "cl-tohoku/bert-base-japanese-whole-word-masking"
 
-MODEL_PATH = "../models/model_{}.bin".format(MODEL_NUM)
+MODEL_PATH = "../models/"
+OUTPUT_PATH = "../output/"
 
 MAX_LEN = 512
 
@@ -20,6 +29,10 @@ VALID_BATCH_SIZE = 16
 
 ENCODER_LR = 2e-5
 DECODER_LR = 2e-5
+
+DROP_OUT = 0.2
+
+WEIGHT_DECAY = 0.01
 
 PRINT_FREQ = 50
 
